465645fef98a337438638a20170789713542a7c6
khian <khian@khiannecert19>
2023-10-12 14:45:30 +0800

    EXAM 3 yee

---
+++ src/certifico_exam_final/Certifico_Exam_Final.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package certifico_exam_final;
+
+/**
+ *
+ * @author khian
+ */
+public class Certifico_Exam_Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
